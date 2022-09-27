af3e64cea0d4b2f8c0ff38f294e2ee8d99420014
Uljas Wennström <uljas.wennstrom@gmail.com>
2022-09-27 18:31:41 +0200

    search

--- src/components/Delete.jsx
+++ src/components/Delete.jsx
@@ -2,10 +2,25 @@ import React from 'react'
 import { Button } from 'react-bootstrap'
 
 export default function Delete() {
-  return (
+  async function deleteUser(e) {
+    e.preventDefault()
+
+    const response = await fetch("http://demonewspaper-001-site1.btempurl.com/api/Users/delete", {
+      method: 'DELETE',
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem("AccessToken")}`
+       },
+      });
+      
+    alert(await response.json());
+  }
+
+  return (    
     <div>
         <h2>Delete account</h2>
-        <Button variant="danger">Delete</Button>
+        <Button variant="danger" onClick={deleteUser}>Delete</Button>
     </div>
   )
 }
